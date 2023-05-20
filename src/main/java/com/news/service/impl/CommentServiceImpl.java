832089b9a05c28e1bc409eff7b8f82cd182a0dd2
justadog <2830978392@qq.com>
2023-05-20 13:50:10 +0800

    CommentService类修改

--- src/main/java/com/news/service/impl/CommentServiceImpl.java
+++ src/main/java/com/news/service/impl/CommentServiceImpl.java
@@ -6,15 +6,52 @@ import com.news.service.CommentService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务实现类
  * </p>
  *
  * @author Sancean
- * @since 2023-05-19
+ * @since 2023-05-18
  */
 @Service
 public class CommentServiceImpl extends ServiceImpl<CommentMapper, Comment> implements CommentService {
+
+    private CommentMapper commentMapper;
+
+    public CommentServiceImpl(CommentMapper commentMapper) {
+        this.commentMapper = commentMapper;
+    }
+
+    public List<Comment> getAll(){
+        List<Comment> commentList=commentMapper.selectList(null);
+//        QueryWrapper<Comment> wrapper = new QueryWrapper<>();
+//        wrapper.eq("user", "晓得");
+//        List<Comment> list=commentMapper.selectList(wrapper);
+        return commentList;
+    }
+
+    @Override
+    public boolean insert(Comment comment) {
+        return commentMapper.insert(comment)>0;
+    }
+
+    @Override
+    public boolean updateId(Comment comment) {
+        return commentMapper.updateById(comment)>0;
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        return commentMapper.deleteById(id)>0;
+    }
+
+    @Override
+    public boolean batch_delete(List<Integer> idList) {
+        return commentMapper.deleteBatchIds(idList)>0;
+    }
+
 
 }
