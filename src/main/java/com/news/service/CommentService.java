51311e612752f963f66a4ec736047739041d0e8a
justadog <2830978392@qq.com>
2023-05-20 13:42:32 +0800

    CommentService类修改

--- src/main/java/com/news/service/CommentService.java
+++ src/main/java/com/news/service/CommentService.java
@@ -3,14 +3,30 @@ package com.news.service;
 import com.news.entity.Comment;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务类
  * </p>
  *
  * @author Sancean
- * @since 2023-05-19
+ * @since 2023-05-18
  */
 public interface CommentService extends IService<Comment> {
 
+    //查询全部
+    public List<Comment> getAll();
+
+    //添加
+    public boolean insert(Comment comment);
+
+    //根据Id修改
+    public boolean updateId(Comment comment);
+
+    //根据Id删除
+    public boolean delete(Integer id);
+
+    //批量删除
+    public boolean batch_delete(List<Integer> idList);
 }
