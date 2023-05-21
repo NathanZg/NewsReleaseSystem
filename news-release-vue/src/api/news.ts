90770faac9da691190740026d6e750210d4d08b0
Sancean <246288805@qq.com>
2023-05-21 21:31:19 +0800

    chore(api): 完成新闻、新闻类型CRUD接口编写

---
+++ news-release-vue/src/api/news.ts
@@ -0,0 +1,35 @@
+import httpService from "@/request"
+export function newsAdd(news: object) {
+    return httpService({
+        url: 'newsAdd',
+        method: 'post',
+        data: news
+    })
+}
+
+export function newsDelete(ids: string) {
+    return httpService({
+        url: 'newsDelete',
+        method: 'delete',
+        data: ids,
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    })
+}
+
+export function newsSelect(queryVo: object) {
+    return httpService({
+        url: 'newsSelect',
+        method: 'post',
+        data: queryVo
+    })
+}
+
+export function newsUpdate(news: object) {
+    return httpService({
+        url: 'newsUpdate',
+        method: 'post',
+        data: news
+    })
+}
