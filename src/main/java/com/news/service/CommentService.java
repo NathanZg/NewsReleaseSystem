8bee88b98cf43fd142d3d21c25ad7db1fc5e4938
Sancean <246288805@qq.com>
2023-05-21 19:56:14 +0800

    style: 文档注释修改2

--- src/main/java/com/news/service/CommentService.java
+++ src/main/java/com/news/service/CommentService.java
@@ -24,28 +24,28 @@ public interface CommentService extends IService<Comment> {
 
     /**
      * 添加
-     * @param Comment comment 评论信息
+     * @param comment 评论信息
      * @return 删除成功(true)/删除失败(false)
      */
     public boolean insert(Comment comment);
 
     /**
      * 根据Id修改
-     * @param Comment comment 评论信息
+     * @param comment 评论信息
      * @return 删除成功(true)/删除失败(false)
      */
     public boolean updateId(Comment comment);
 
     /**
      * 根据Id删除
-     * @param Integer id 评论id
+     * @param id 评论id
      * @return 删除成功(true)/删除失败(false)
      */
     public boolean delete(Integer id);
 
     /**
      * 批量删除
-     * @param List<Integer> idList 评论id列表
+     * @param 评论id列表
      * @return 删除成功(true)/删除失败(false)
      */
     public boolean batch_delete(String deleteIds);
